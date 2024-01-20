46f20cddf981020f6901325be21e6269ba3a4fc5
syhuh <syhuh1125@gmail.com>
2024-01-20 13:11:22 +0900

    First Version to Deploy on Vercel

---
+++ app/page.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import Heading from "@/components/Heading";
+import { getFeaturedReview } from "@/lib/reviews";
+
+export default async function HomePage() {
+  console.log("[HomePage] rendering");
+  const review = await getFeaturedReview();
+  console.log("[HomePage] review: ", review);
+
+  return (
+    <>
+      <Heading>{review.title}</Heading>
+      <p className="pb-3">Only the best indie games, reviewed for you.</p>
+      <div
+        className="bg-white border rounded shadow w-80
+                      hover:shadow-xl sm:w-full"
+      >
+        <Link
+          href={`/reviews/${review.slug}`}
+          className="flex flex-col sm:flex-row"
+        >
+          <img
+            src={review.image}
+            alt=""
+            width="320"
+            height="180"
+            className="rounded-t sm:rounded-l sm:rounded-r-none"
+          />
+          <h2 className="font-orbitron font-semibold py-1 text-center sm:px-2">
+            {review.title}
+          </h2>
+        </Link>
+      </div>
+    </>
+  );
+}
---
+++ app/reviews/page.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import Heading from "@/components/Heading";
+import { getReviews } from "@/lib/reviews";
+
+export const metadata = {
+  title: "Reviews",
+};
+
+export default async function ReviewsPage() {
+  const reviews = await getReviews();
+
+  return (
+    <>
+      <Heading>Reviews</Heading>
+      <ul className="flex flex-row flex-wrap gap-3">
+        {reviews.map((review) => (
+          <li className="bg-white border rounded shadow w-80 hover:shadow-xl">
+            <Link href={`/reviews/${review.slug}`}>
+              <img
+                src={review.image}
+                alt=""
+                width="320"
+                height="180"
+                className="rounded-t"
+              />
+              <h2 className="font-orbitron font-semibold py-1 text-center">
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
